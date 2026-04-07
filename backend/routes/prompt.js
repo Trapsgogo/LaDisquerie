eef035735d3f1950e7594e7dcbc657615e7aec9c
Trapsgogo <hugoboubault@gmail.com>
2026-04-07 11:48:29 +0200

    Delete backend directory

--- backend/routes/prompt.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const promptController = require('../controllers/promptController');
-
-const router = express.Router();
-
-router.post('/add', promptController.addPrompt);
-router.get('/all', promptController.getAllPrompts);
-
-module.exports = router;
